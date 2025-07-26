9c8b09f327e2cdf7c68694bf206968fcd9a7efbb
melnard <dejesusmelnard@gmail.com>
2025-07-27 01:37:36 +0800

    revise middlewae

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,4 @@
-// middleware.ts
-import { getToken } from 'next-auth/jwt';
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import NextAuth from 'next-auth';
+import authConfig from './auth.config';
 
-export async function middleware(req: NextRequest) {
-  const token = await getToken({ req, secret: process.env.AUTH_SECRET });
-  if (req.nextUrl.pathname.startsWith('/admin')) {
-    if (!token || token.role !== 'ADMIN') {
-      console.log('token in middleware admin', token);
-
-      return NextResponse.redirect(new URL('/unauthorized', req.url));
-    }
-  }
-
-  return NextResponse.next();
-}
-
-// Limit it only to the needed paths
-export const config = {
-  matcher: ['/admin/:path*'],
-};
+export const { auth: middleware } = NextAuth(authConfig);
